54acbda90314901bcf14d6fbc686d6746790f2ae
ntoti-cy <cynthialeyain20@gmail.com>
2026-08-06 19:32:01 +0300

    SMS Testing 3

--- app/routes/user_crud_routes.py
+++ app/routes/user_crud_routes.py
@@ -565,7 +565,7 @@ def ussd_callback():
                         )
                         # Build Sms Message
                         expiry = (
-                            center.session_end.strftime("%Y-%m-%d %H:%M:%S")
+                            center.expiry_time.strftime("%Y-%m-%d %H:%M:%S")
                             if center.expiry_time
                             else "Not specified"
                         )
